8a1d4bb12d797d1f590d2f75e8a743cd31e10bde
umxe <u_habiba@rocketmail.com>
2018-06-26 14:06:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+get "/name" do
+  "My name is Ume"
+end
+
+get "/hometown" do
+  "My hometown is Baltimore, Maryland"
+end
+  
+get "/favorite-song" do
+  "My favorite song is One Thing"
+end
+  
 end
